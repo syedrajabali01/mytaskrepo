7308cd9ed5fd8671bfe1cc36f692d3c81a92cc90
Rajab <syedrajabali@gmail.com>
2022-10-02 09:35:02 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Card  from 'react-bootstrap/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button></Button><br>
+      </br>
+     <Button >Click me</Button> 
+     <h1>Rajab </h1>
+     <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="logo192.png" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+     
+
+
     </div>
   );
 }
