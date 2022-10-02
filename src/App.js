83d20e8562dc60ba1d19b6d3c5c840041c7c698b
Rajab <syedrajabali@gmail.com>
2022-10-02 09:39:20 +0500

    card title

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
      <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="logo192.png" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Card </Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
